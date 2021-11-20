abd54925fce15a9555e41614a30ffde2215d276b
Jesse Garcilazo <jjgarcilazo@gmail.com>
2021-11-19 17:56:01 -0800

    Break Bread Son

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,61 +1,77 @@
 import React from 'react';
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,} from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-
-
- function RenderCampsite({campsite}) {
+function RenderCampsite({ campsite }) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+function RenderComments({ comments }) {
+  if (comments) {
     return (
       <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+        <h4>Comments</h4>
+        {comments.map(comment => {
+          return (
+            <div key={comment.id}>
+              <div>{comment.text}</div>
+              <div>
+                -- {comment.author},{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit',
+                }).format(new Date(Date.parse(comment.date)))}
+              </div>
+              <br />
+            </div>
+          );
+        })}
       </div>
     );
   }
-  function RenderComments({comments}) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map(comment => {
-            return (
-              <div key={comment.id}>
-                <div>{comment.text}</div>
-                <div>
-                  -- {comment.author},{' '}
-                  {new Intl.DateTimeFormat('en-US', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: '2-digit',
-                  }).format(new Date(Date.parse(comment.date)))}
-                </div>
-                <br />
-              </div>
-            );
-          })}
+  return <div />;
+}
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/directory">Directory</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
         </div>
-      );
-    }
-    return <div />;
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.comments} />
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
-
-  function CampsiteInfo(props) {
-      if (props.campsite) {
-          return (
-              <div className="container">
-                  <div className="row">
-                      <RenderCampsite campsite={props.campsite} />
-                      <RenderComments comments={props.comments} />
-                  </div>
-              </div>
-          );
-      }
-      return <div />;
-  }
-  
-  export default CampsiteInfo;
+}
+export default CampsiteInfo;
